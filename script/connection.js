9c29d84b30005108feed4cb5cb71aa252f4d1340
Ueslei Amaral <95388053+UesleiAmaral@users.noreply.github.com>
2023-05-31 20:10:36 -0300

    Update connection.js

--- script/connection.js
+++ script/connection.js
@@ -2,7 +2,11 @@ const url = 'https://jogo-da-forca-gules-ten.vercel.app/db.json';
 
 export const db = async () => {
   const endpoint = `${url}/db.json`;
-  const data = await fetch(endpoint);
+  const data = await fetch(endpoint, { mode: 'cors',
+  headers: {
+    'Access-Control-Allow-Origin':'*'
+  }}
+);
   const palavras = data.json();
 
   return palavras;
